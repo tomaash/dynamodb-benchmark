1259eb4ac8b25297ea3252e45d01ffcc9b3f9bfa
Tomas Holas <tomas.holas@gmail.com>
2015-07-28 10:21:08 +0200

    fix indentation

--- server/config.example.js
+++ server/config.example.js
@@ -1,11 +1,11 @@
 module.exports = {
   aws: {
-		"accessKeyId": "YOUR_ACCESS_KEY_ID",
-  	"secretAccessKey": "YOUR_SECRET_ACCESS_KEY",
-  	"region": "us-east-1"
+    "accessKeyId": "YOUR_ACCESS_KEY_ID",
+    "secretAccessKey": "YOUR_SECRET_ACCESS_KEY",
+    "region": "us-east-1"
   },
   dynamise: {
-  	endpoint: "local"
+    endpoint: "local"
   },
   server: {
       port: 3000
